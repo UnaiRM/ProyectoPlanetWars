e40922e7189749ba9d7e8ecc4bfbb3ca9178e1d7
UnaiRM <miperritolukas@gmail.com>
2022-05-05 11:27:06 +0200

    Crear y eliminar base de datos
    Subo crear en mi branch y la parte del drop separada

---
+++ M02/Create_DDBB.sql
@@ -0,0 +1,72 @@
+CREATE TABLE USERS
+  (ID_user numeric primary key,
+  username varchar(30) unique,
+  birth_date date,
+  password varchar(30));
+
+CREATE TABLE PLANET
+  (ID_planet numeric primary key,
+  ID_user numeric,
+  name varchar(30) unique,
+  technology_atack numeric,
+  technology_defense numeric,
+  update_defense_cost numeric,
+  update_atack_cost numeric,
+  crystal_quantity numeric,
+  iron_quantity numeric,
+  deuterium_quantity numeric,
+  CONSTRAINT Planet_User_fk FOREIGN KEY (ID_user) REFERENCES USERS (ID_user));
+  
+
+CREATE TABLE SHIP
+  (ID_ship numeric primary key,
+  name varchar(30) unique,
+  metal_cost numeric,
+  crystal_cost numeric,
+  deuterium_cost numeric,
+  initial_armor numeric,
+  armor numeric,
+  base_damage numeric,
+  speed numeric,
+  generate_wastings numeric);
+  
+CREATE TABLE PLANET_SHIP
+  (ID_planet numeric,
+  ID_ship numeric,
+  cuantity numeric,
+   CONSTRAINT Planet_Ship_fk FOREIGN KEY (ID_planet) REFERENCES PLANET (ID_planet),
+   CONSTRAINT Ship_Planet_fk FOREIGN KEY (ID_ship) REFERENCES SHIP (ID_ship),
+   PRIMARY KEY(ID_planet, ID_ship));
+
+CREATE TABLE DEFENSE
+  (ID_defense numeric primary key,
+  name varchar(30) unique,
+  metal_cost numeric,
+  crystal_cost numeric,
+  deuterium_cost numeric,
+  initial_armor numeric,
+  armor numeric,
+  base_damage numeric,
+  speed numeric,
+  generate_wastings numeric);
+
+CREATE TABLE PLANET_DEFENSE
+  (ID_planet numeric,
+  ID_defense numeric,
+  cuantity numeric,
+  CONSTRAINT Planet_Defense_fk FOREIGN KEY (ID_planet) REFERENCES PLANET (ID_planet),
+  CONSTRAINT Defense_Planet_fk FOREIGN KEY (ID_defense) REFERENCES DEFENSE (ID_defense),
+  PRIMARY KEY(ID_planet, ID_defense));
+
+CREATE TABLE BATTLE
+  (ID_battle numeric primary key,
+  ID_user numeric,
+  unit_user_begin numeric,
+  unit_user_end numeric,
+  unit_enemy_begin numeric,
+  unit_enemy_end numeric,
+  CONSTRAINT Battle_User_fk FOREIGN KEY (ID_user) REFERENCES USERS (ID_user));
+
+  Commit;
+  
+ 
