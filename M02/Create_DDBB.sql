449e0c74643230782e7dcdc8fd0a938c33799172
UnaiRM <miperritolukas@gmail.com>
2022-05-06 11:40:10 +0200

    Pasar a procedures el create, insert y drop
    Creacion de los procedures a partir del create, insert y drop

--- M02/Create_DDBB.sql
+++ M02/Create_DDBB.sql
@@ -1,9 +1,21 @@
-CREATE TABLE USERS
+create or replace procedure CREATE_TABLE_02
+as
+     create1 VARCHAR(500);
+     create2 VARCHAR(500);
+     create3 VARCHAR(500);
+     create4 VARCHAR(500);
+     create5 VARCHAR(500);
+     create6 VARCHAR(500);
+     create7 VARCHAR(500);
+     
+begin
+    create1 := 'CREATE TABLE USERS
   (ID_user numeric primary key,
   username varchar(30) unique,
   birth_date date,
-  password varchar(30));
-
+  password varchar(30))';
+  
+    create2 := '
 CREATE TABLE PLANET
   (ID_planet numeric primary key,
   ID_user numeric,
@@ -15,9 +27,9 @@ CREATE TABLE PLANET
   crystal_quantity numeric,
   iron_quantity numeric,
   deuterium_quantity numeric,
-  CONSTRAINT Planet_User_fk FOREIGN KEY (ID_user) REFERENCES USERS (ID_user));
+  CONSTRAINT Planet_User_fk FOREIGN KEY (ID_user) REFERENCES USERS (ID_user))';
   
-
+    create3 := '
 CREATE TABLE SHIP
   (ID_ship numeric primary key,
   name varchar(30) unique,
@@ -28,16 +40,19 @@ CREATE TABLE SHIP
   armor numeric,
   base_damage numeric,
   speed numeric,
-  generate_wastings numeric);
+  generate_wastings numeric)';
   
+    create4 := '
 CREATE TABLE PLANET_SHIP
   (ID_planet numeric,
   ID_ship numeric,
   cuantity numeric,
+  lvl numeric,
    CONSTRAINT Planet_Ship_fk FOREIGN KEY (ID_planet) REFERENCES PLANET (ID_planet),
    CONSTRAINT Ship_Planet_fk FOREIGN KEY (ID_ship) REFERENCES SHIP (ID_ship),
-   PRIMARY KEY(ID_planet, ID_ship));
-
+   PRIMARY KEY(ID_planet, ID_ship))';
+   
+    create5 := '
 CREATE TABLE DEFENSE
   (ID_defense numeric primary key,
   name varchar(30) unique,
@@ -48,16 +63,19 @@ CREATE TABLE DEFENSE
   armor numeric,
   base_damage numeric,
   speed numeric,
-  generate_wastings numeric);
+  generate_wastings numeric)';
 
+    create6 := '
 CREATE TABLE PLANET_DEFENSE
   (ID_planet numeric,
   ID_defense numeric,
   cuantity numeric,
+  lvl numeric,
   CONSTRAINT Planet_Defense_fk FOREIGN KEY (ID_planet) REFERENCES PLANET (ID_planet),
   CONSTRAINT Defense_Planet_fk FOREIGN KEY (ID_defense) REFERENCES DEFENSE (ID_defense),
-  PRIMARY KEY(ID_planet, ID_defense));
+  PRIMARY KEY(ID_planet, ID_defense))';
 
+    create7:= '
 CREATE TABLE BATTLE
   (ID_battle numeric primary key,
   ID_user numeric,
@@ -65,8 +83,19 @@ CREATE TABLE BATTLE
   unit_user_end numeric,
   unit_enemy_begin numeric,
   unit_enemy_end numeric,
-  CONSTRAINT Battle_User_fk FOREIGN KEY (ID_user) REFERENCES USERS (ID_user));
-
-  Commit;
+  CONSTRAINT Battle_User_fk FOREIGN KEY (ID_user) REFERENCES USERS (ID_user))';
   
+  EXECUTE IMMEDIATE create1;
+  EXECUTE IMMEDIATE create2;
+  EXECUTE IMMEDIATE create3;
+  EXECUTE IMMEDIATE create4;
+  EXECUTE IMMEDIATE create5;
+  EXECUTE IMMEDIATE create6;
+  EXECUTE IMMEDIATE create7;
+  end;
+  /
+  set serveroutput on
+  begin
+  CREATE_TABLE_02;
+  end;
  
