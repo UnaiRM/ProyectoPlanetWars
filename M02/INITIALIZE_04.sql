c0c83be293cd8556fe0ad34d7dab529dcbb92400
AndresVT24 <95907514+AndresVT24@users.noreply.github.com>
2022-05-09 12:10:09 +0200

    Update INITIALIZE_04.sql

--- M02/INITIALIZE_04.sql
+++ M02/INITIALIZE_04.sql
@@ -1,20 +1,42 @@
-create or replace procedure INITIALIZE_04 (boton_reset int)
-is
-   compr int;
-
-begin
-   select count(table_name) into compr from user_tables;
+create or replace procedure INITIALIZE_04
+as
+   boton_reset int := 0;
    
-   if boton_reset = 1 then
-      DROP_TABLE_01;
+   existe_b int;
+   existe_d int;
+   existe_p int;
+   existe_pd int;
+   existe_ps int;
+   existe_s int;
+   existe_u int;
+   total int;
+   
+begin
+   select count(*) into existe_b from tab where tname=upper('BATTLE');
+   select count(*) into existe_d from tab where tname=upper('DEFENSE');
+   select count(*) into existe_p from tab where tname=upper('PLANET');
+   select count(*) into existe_pd from tab where tname=upper('PLANET_DEFENSE');
+   select count(*) into existe_ps from tab where tname=upper('PLANET_SHIP');
+   select count(*) into existe_s from tab where tname=upper('SHIP');
+   select count(*) into existe_u from tab where tname=upper('USERS');
+   
+   total := existe_b + existe_d + existe_p + existe_pd + existe_ps + existe_s + existe_u;
+   
+   if total != 7 then
+      DBMS_OUTPUT.PUT_LINE('CREATE, INSERT');
       CREATE_TABLE_02;
+      DBMS_OUTPUT.put_line('SE HA CREADO LA TABLA 0');
       INSERT_DATA_03;
-      
-   else
-      if compr = 0 then
+      DBMS_OUTPUT.put_line('SE HA INSERTADO LA TABLA 0');
+   else--PARA EL BOTON RESET
+      if boton_reset = 1 then
+         DBMS_OUTPUT.put_line('BOTON RESET');
          DROP_TABLE_01;
+         DBMS_OUTPUT.put_line('SE HA BORRADO LA TABLA 1');
          CREATE_TABLE_02;
+         DBMS_OUTPUT.put_line('SE HA CREADO LA TABLA 1');
          INSERT_DATA_03;
+         DBMS_OUTPUT.put_line('SE HA INSERTADO LA TABLA 1');
          
       end if;
    end if;
