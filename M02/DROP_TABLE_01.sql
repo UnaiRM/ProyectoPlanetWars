859422bccdc3287e39c4fbcce80dd3d8218fa2fb
AndresVT24 <95907514+AndresVT24@users.noreply.github.com>
2022-05-10 11:35:07 +0200

    Add files via upload

---
+++ M02/DROP_TABLE_01.sql
@@ -0,0 +1,18 @@
+create or replace procedure DROP_TABLE_01
+as
+     
+begin
+
+   execute immediate  'DROP TABLE BATTLE';
+   execute immediate  'DROP TABLE PLANET_SHIP';
+   execute immediate  'DROP TABLE PLANET_DEFENSE';
+   execute immediate  'DROP TABLE DEFENSE';
+   execute immediate  'DROP TABLE SHIP';
+   execute immediate  'DROP TABLE PLANET';
+   execute immediate  'DROP TABLE USERS';
+
+exception
+   when others then
+   dbms_output.put_line('ERROR: NO SE HA INDENTIFICADO EL ERROR DROP TABLE');
+
+end;
