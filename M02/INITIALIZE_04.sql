b8b010e5ad25a89be6caa21afc9c969f5d065687
AndresVT24 <95907514+AndresVT24@users.noreply.github.com>
2022-05-10 11:23:16 +0200

    Update INITIALIZE_04.sql

--- M02/INITIALIZE_04.sql
+++ M02/INITIALIZE_04.sql
@@ -1,7 +1,5 @@
-create or replace procedure INITIALIZE_04
-as
-   boton_reset int := 0;
-   
+create or replace procedure INITIALIZE_04(boton_reset int)
+as   
    existe_b int;
    existe_d int;
    existe_p int;
@@ -21,23 +19,30 @@ begin
    select count(*) into existe_u from tab where tname=upper('USERS');
    
    total := existe_b + existe_d + existe_p + existe_pd + existe_ps + existe_s + existe_u;
-   
+
    if total != 7 then
-      DBMS_OUTPUT.PUT_LINE('CREATE, INSERT');
+      --PARA CUANDO SE INICIA EL PROGRAMA
       CREATE_TABLE_02;
-      DBMS_OUTPUT.put_line('SE HA CREADO LA TABLA 0');
+      DBMS_OUTPUT.put_line('SE HA CREADO LA TABLA');
       INSERT_DATA_03;
-      DBMS_OUTPUT.put_line('SE HA INSERTADO LA TABLA 0');
-   else--PARA EL BOTON RESET
+      DBMS_OUTPUT.put_line('SE HA INSERTADO LA TABLA');
+      
+   else
+      --PARA EL BOTON RESET
       if boton_reset = 1 then
          DBMS_OUTPUT.put_line('BOTON RESET');
          DROP_TABLE_01;
-         DBMS_OUTPUT.put_line('SE HA BORRADO LA TABLA 1');
+         DBMS_OUTPUT.put_line('SE HA BORRADO LA TABLA, BOTON RESET');
          CREATE_TABLE_02;
-         DBMS_OUTPUT.put_line('SE HA CREADO LA TABLA 1');
+         DBMS_OUTPUT.put_line('SE HA CREADO LA TABLA, BOTON RESET');
          INSERT_DATA_03;
-         DBMS_OUTPUT.put_line('SE HA INSERTADO LA TABLA 1');
+         DBMS_OUTPUT.put_line('SE HA INSERTADO LA TABLA, BOTON RESET');
          
       end if;
    end if;
+   
+exception
+   when others then
+   dbms_output.put_line('ERROR: NO SE HA INDENTIFICADO EL ERROR INSERT DATA');
+   
 end;
