2256b187739fbf1a380f7f370dc5017cf523a15d
AndresVT24 <andres24vt@gmail.com>
2022-05-12 11:42:38 +0200

    INITIALIZE

--- M02/CREATE_TABLE_02.sql
+++ M02/CREATE_TABLE_02.sql
@@ -2,45 +2,44 @@ create or replace procedure CREATE_TABLE_02
 as
    
 begin
-  
    EXECUTE IMMEDIATE 'CREATE TABLE USERS
    (ID_user numeric primary key,
-   username varchar(30) unique,
-   birth_date date,
-   password varchar(30))';
+   username varchar(30) unique not null,
+   birth_date date not null,
+   password varchar(30) not null)';
    
    EXECUTE IMMEDIATE 'CREATE TABLE PLANET
    (ID_planet numeric primary key,
    ID_user numeric not null,
-   name varchar(30) unique,
-   technology_atack numeric,
-   technology_defense numeric,
-   update_defense_cost numeric,
-   update_atack_cost numeric,
-   crystal_quantity numeric,
-   metal_quantity numeric,
-   deuterium_quantity numeric,
+   name varchar(30) unique not null,
+   technology_atack numeric not null,
+   technology_defense numeric not null,
+   update_defense_cost numeric not null,
+   update_atack_cost numeric not null,
+   crystal_quantity numeric not null,
+   metal_quantity numeric not null,
+   deuterium_quantity numeric not null,
    CONSTRAINT Planet_User_fk FOREIGN KEY (ID_user)
       REFERENCES USERS (ID_user))';
    
    EXECUTE IMMEDIATE 'CREATE TABLE SHIP
    (ID_ship numeric primary key,
-   name varchar(30) unique,
-   metal_cost numeric,
-   crystal_cost numeric,
-   deuterium_cost numeric,
-   initial_armor numeric,
-   armor numeric,
-   base_damage numeric,
-   speed numeric,
-   generate_wastings numeric)';
+   name varchar(30) unique not null,
+   metal_cost numeric not null,
+   crystal_cost numeric not null,
+   deuterium_cost numeric not null,
+   initial_armor numeric not null,
+   armor numeric not null,
+   base_damage numeric not null,
+   speed numeric not null,
+   generate_wastings numeric not null)';
    
    EXECUTE IMMEDIATE 'CREATE TABLE PLANET_SHIP
-   (ID_planet numeric,
-   ID_ship numeric,
-   quantity numeric,
-   level_defense numeric,
-   level_atack numeric,
+   (ID_planet numeric not null,
+   ID_ship numeric not null,
+   quantity numeric not null,
+   level_defense numeric not null,
+   level_atack numeric not null,
    CONSTRAINT Planet_Ship_fk FOREIGN KEY (ID_planet)
       REFERENCES PLANET (ID_planet),
    CONSTRAINT Ship_Planet_fk FOREIGN KEY (ID_ship)
@@ -49,22 +48,22 @@ begin
    
    EXECUTE IMMEDIATE 'CREATE TABLE DEFENSE
    (ID_defense numeric primary key,
-   name varchar(30) unique,
-   metal_cost numeric,
-   crystal_cost numeric,
-   deuterium_cost numeric,
-   initial_armor numeric,
-   armor numeric,
-   base_damage numeric,
-   speed numeric,
-   generate_wastings numeric)';
+   name varchar(30) unique not null,
+   metal_cost numeric not null,
+   crystal_cost numeric not null,
+   deuterium_cost numeric not null,
+   initial_armor numeric not null,
+   armor numeric not null,
+   base_damage numeric not null,
+   speed numeric not null,
+   generate_wastings numeric not null)';
    
    EXECUTE IMMEDIATE 'CREATE TABLE PLANET_DEFENSE
-   (ID_planet numeric,
-   ID_defense numeric,
-   quantity numeric,
-   level_defense numeric,
-   level_atack numeric,
+   (ID_planet numeric not null,
+   ID_defense numeric not null,
+   quantity numeric not null,
+   level_defense numeric not null,
+   level_atack numeric not null,
    CONSTRAINT Planet_Defense_fk FOREIGN KEY (ID_planet)
       REFERENCES PLANET (ID_planet),
    CONSTRAINT Defense_Planet_fk FOREIGN KEY (ID_defense)
@@ -73,17 +72,19 @@ begin
    
    EXECUTE IMMEDIATE 'CREATE TABLE BATTLE
    (ID_battle numeric primary key,
-   ID_user numeric,
-   unit_user_begin numeric,
-   unit_user_end numeric,
-   unit_enemy_begin numeric,
-   unit_enemy_end numeric,
+   ID_user numeric not null,
+   unit_user_begin numeric not null,
+   unit_user_end numeric not null,
+   unit_enemy_begin numeric not null,
+   unit_enemy_end numeric not null,
    CONSTRAINT Battle_User_fk FOREIGN KEY (ID_user)
       REFERENCES USERS (ID_user))';
+   commit;
 
 exception
    when others then
    dbms_output.put_line('ERROR: NO SE HA INDENTIFICADO EL ERROR CREATE TABLE');
-  
+   rollback;
+   
 end;
  
