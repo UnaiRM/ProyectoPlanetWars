25a8729dcdcfc8f472fd4b72aab7a986d46c3d86
AndresVT24 <andres24vt@gmail.com>
2022-05-17 11:59:17 +0200

    CAMBIOS

--- M02/PACKAGE_PLANET_WARS.sql
+++ M02/PACKAGE_PLANET_WARS.sql
@@ -45,11 +45,11 @@ as
    
    procedure NEXT_ID(next_id out int);
    
-   procedure RETURN_PLANET_SHIP(id_p in int, all_c out varchar);
+   procedure RETURN_PLANET_SHIP(id_p int, res out varchar);
    
-   procedure RETURN_PLANET_DEFENSE(id_p in int, all_c out varchar);
+   procedure RETURN_PLANET_DEFENSE(id_p int, res out varchar);
 
-    procedure INSERT_USER(nombre users.username%type, pas users.password%type, res out int);
+   procedure INSERT_USER(nombre users.username%type, pas users.password%type, res out int);
     
 end;
 /
@@ -683,7 +683,6 @@ as
    
    end;
    -------------------------------------------------------------------------------------------------------------------
-   
     procedure INSERT_USER(nombre users.username%type, pas users.password%type, res out int)
     as
         cantidadUsuarios int;
@@ -713,9 +712,116 @@ as
     exception
         when nombre_existe_exc then
         dbms_output.put_line('ERROR: YA EXISTE UN USUARIO CON ESE NOMRE');
-        when others then
-            dbms_output.put_line('ERROR: Fallo desconocido');
+        
+        when others then 
+        dbms_output.put_line('ERROR: NO SE HA INDENTIFICADO EL ERROR INSERT_USER');
     
     end;
-
+-----------------------------------------------------------------------------------------------------------------------------
+   procedure RETURN_PLANET_SHIP(id_p int, res out varchar)
+   as
+      cursor c1 is select id_ship, quantity, level_defense, level_atack from planet_ship where id_planet = id_p;
+      all_c varchar(500);
+      id_d int;
+      quant int;
+      lvl_d int;
+      lvl_s int;
+      
+      ver_id int;
+      excp_id exception;
+      
+   begin
+      select count(id_planet) into ver_id from planet_ship where id_planet = id_p;
+      if ver_id = 0 then
+         raise excp_id;
+         
+      end if;
+      
+      open c1;
+      loop
+         all_c := '0';
+         fetch c1 into id_d, quant, lvl_d, lvl_s;
+            exit when c1%notfound;
+               if all_c = '0' then
+                  all_c := to_char(id_d);
+                  all_c := all_c||','||to_char(quant);
+                  all_c := all_c||','||to_char(lvl_d);
+                  all_c := all_c||','||to_char(lvl_s);
+                  all_c := all_c||';';
+                  
+                  if res is null then
+                     res := all_c;
+                  
+                  else
+                     res := res||all_c;
+                     
+                  end if;
+               end if;
+      end loop;
+      close c1;
+   
+   exception
+      when excp_id then
+      DBMS_OUTPUT.PUT_LINE('EL ID_PLANET '||id_p||' NO EXISTE');
+      rollback;
+      
+      when others then
+      dbms_output.put_line('ERROR: NO SE HA INDENTIFICADO EL ERROR RETURN_PLANET_SHIP');
+      rollback;
+      
+   end;
+--------------------------------------------------------------------------------------------------------
+   procedure RETURN_PLANET_DEFENSE(id_p int, res out varchar)
+   as
+      cursor c1 is select id_defense, quantity, level_defense, level_atack from planet_defense where id_planet = id_p;
+      all_c varchar(500);
+      id_d int;
+      quant int;
+      lvl_d int;
+      lvl_s int;
+      
+      ver_id int;
+      excp_id exception;
+      
+   begin
+      select count(id_planet) into ver_id from planet_defense where id_planet = id_p;
+      if ver_id = 0 then
+         raise excp_id;
+         
+      end if;
+      
+      open c1;
+      loop
+         all_c := '0';
+         fetch c1 into id_d, quant, lvl_d, lvl_s;
+            exit when c1%notfound;
+               if all_c = '0' then
+                  all_c := to_char(id_d);
+                  all_c := all_c||','||to_char(quant);
+                  all_c := all_c||','||to_char(lvl_d);
+                  all_c := all_c||','||to_char(lvl_s);
+                  all_c := all_c||';';
+                  
+                  if res is null then
+                     res := all_c;
+                  
+                  else
+                     res := res||all_c;
+                     
+                  end if;
+               end if;
+      end loop;
+      close c1;
+   
+   exception
+      when excp_id then
+      DBMS_OUTPUT.PUT_LINE('EL ID_PLANET '||id_p||' NO EXISTE');
+      rollback;
+      
+      when others then
+      dbms_output.put_line('ERROR: NO SE HA INDENTIFICADO EL ERROR RETURN_PLANET_DEFENSE');
+      rollback;
+      
+   end;
+------------------------------------------------------------------------------------------------------------
 end;
--- M02/RETURN_PLANET_SHIP.sql
+++ M02/RETURN_PLANET_SHIP.sql
@@ -1,89 +1,52 @@
-create or replace procedure RETURN_PLANET_SHIP(id_p in int, all_c out varchar)
+create or replace procedure RETURN_PLANET_SHIP(id_p int, res out varchar)
 as
-   cursor c1 is select id_ship from planet_ship where id_planet = id_p;
-   cursor c2 is select quantity from planet_ship where id_planet = id_p;
-   cursor c3 is select level_defense from planet_ship where id_planet = id_p;
-   cursor c4 is select level_atack from planet_ship where id_planet = id_p;
-   x int;
+   cursor c1 is select id_ship, quantity, level_defense, level_atack from planet_ship where id_planet = id_p;
+   all_c varchar(500);
+   id_d int;
+   quant int;
+   lvl_d int;
+   lvl_s int;
    
    ver_id int;
    excp_id exception;
+   
 begin
    select count(id_planet) into ver_id from planet_ship where id_planet = id_p;
-   
    if ver_id = 0 then
       raise excp_id;
+      
    end if;
---CURSOR PARA LA ID_SHIP
+   
    open c1;
    loop
-      fetch c1 into x;
+      all_c := '0';
+      fetch c1 into id_d, quant, lvl_d, lvl_s;
          exit when c1%notfound;
-         if all_c is null then
-             all_c := to_char(x);
-            
-         else
-            all_c := all_c||','||to_char(x);
-            
-         end if;
+            if all_c = '0' then
+               all_c := to_char(id_d);
+               all_c := all_c||','||to_char(quant);
+               all_c := all_c||','||to_char(lvl_d);
+               all_c := all_c||','||to_char(lvl_s);
+               all_c := all_c||';';
+               
+               if res is null then
+                  res := all_c;
+               
+               else
+                  res := res||all_c;
+                  
+               end if;
+            end if;
    end loop;
    close c1;
-   
-   all_c := all_c||';';
---CURSOR PARA LA QUANTITY
-   open c2;
-   loop
-      fetch c2 into x;
-         exit when c2%notfound;
-         if substr(all_c,-1,1) = ';' then
-             all_c := all_c ||to_char(x);
-            
-         else
-            all_c := all_c||','||to_char(x);
-         
-      end if;
-   end loop;
-   close c2;
-   
-   all_c := all_c||';';
---CURSOR PARA LEVEL_DEFENSE
-   open c3;
-   loop
-      fetch c3 into x;
-      exit when c3%notfound;
-      if substr(all_c,-1,1) = ';' then
-             all_c := all_c ||to_char(x);
-         
-      else
-         all_c := all_c||','||to_char(x);
-         
-      end if;
-   end loop;
-   close c3;
-   
-   all_c := all_c||';';
---CURSOR PARA LEVEL_ATTACK
-   open c4;
-   loop
-      fetch c4 into x;
-      exit when c4%notfound;
-      if substr(all_c,-1,1) = ';' then
-             all_c := all_c ||to_char(x);
-         
-      else
-         all_c := all_c||','||to_char(x);
-         
-      end if;
-   end loop;
-   
-   close c4;
 
 exception
    when excp_id then
-   DBMS_OUTPUT.PUT_LINE('ERROR: EL ID '||id_p||' NO EXISTE');
-
+   DBMS_OUTPUT.PUT_LINE('EL ID_PLANET '||id_p||' NO EXISTE');
+   rollback;
+   
    when others then
    dbms_output.put_line('ERROR: NO SE HA INDENTIFICADO EL ERROR RETURN_PLANET_SHIP');
    rollback;
-
+   
 end;
